170104c98545e0b5e658ace71404df1c1dd7fbdf
Omer Marquez <omer.marquezt@gmail.com>
2021-09-24 16:29:44 -0400

    test change

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Wenas
+          Wenas 2.0
         </p>
         <a
           className="App-link"
