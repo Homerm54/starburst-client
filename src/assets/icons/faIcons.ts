e6ed556b6a73833d795bf7047ecfce0c88124b6b
Omer Marquez <omer.marquezt@gmail.com>
2022-02-21 22:07:26 -0400

    icon spacing

--- src/assets/icons/faIcons.ts
+++ src/assets/icons/faIcons.ts
@@ -1,5 +1,13 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCircleXmark, faArrowLeft, faArrowRotateLeft } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowLeft,
+  faCircleXmark,
+  faArrowRotateLeft,
+} from '@fortawesome/free-solid-svg-icons';
 
 // Add the icons used by the whole application to the library, allowing to use globally
-library.add(faCircleXmark, faArrowLeft, faArrowRotateLeft);
+library.add(
+  faArrowLeft,
+  faCircleXmark,
+  faArrowRotateLeft,
+);
