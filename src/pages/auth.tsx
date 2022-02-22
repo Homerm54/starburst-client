bbba6816d28e31c0f0c8cc228ffac7c58332a306
Omer Marquez <omer.marquezt@gmail.com>
2022-02-21 22:06:45 -0400

    divider spacing with function

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,23 +1,33 @@
-import { Card } from "components/ui/Card";
-import Console from "lib/Console";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Typography, Divider, Tab, Card } from "components/ui";
 import { useLocation } from 'react-router-dom';
 
-const Auth = () : JSX.Element => {
+const Auth = (): JSX.Element => {
+  
   const state = useLocation().state as { redirectTo: string | null };
   const redirectTo = state?.redirectTo || '';
-  Console.log(`When done, will redirect to: ${redirectTo}`);
-
+ 
   return (
     <div className="m-4">
-      <Card
-        title="Testing a card"
-        actionsTop={<a href="#a">Test this</a>}
-        actionsBottom={[<div key="1">Hola</div>, <div key="2">Hola 2</div>, <div key="3">Hola 3</div>]}
-        // actionsBottom={[<div key="1">Hola</div>]}
-      >
-        <div>Hello, I am a <span className="text-success">Card</span></div>
-        <div>Hello, I am a <span className="text-success">Card</span></div>
-        <div>Hello, I am a <span className="text-success">Card</span></div>
+      <Card style={{ width: '90vw' }}>
+        <Typography variant="h3">Starburst</Typography>
+        <Divider />
+      
+        <Tab.Group initialActiveTab="signin" alignment="center">
+          <Tab.Item
+            icon={<FontAwesomeIcon icon="user-plus" />}
+            tabKey="signin"
+            label="Sign In"
+          >
+            Hola, soy el contenido del tab #1
+          </Tab.Item>
+          <Tab.Item
+            label="Sign Up"
+            tabKey="signup"
+          >
+            Tab #2 aquí
+          </Tab.Item>
+        </Tab.Group>
       </Card>
     </div>
   );
